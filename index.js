88758fad6f0779b95d7e564976976a4e22d6c797
Fahsina24 <fahsinahossain10@gmail.com>
2025-02-25 22:51:49 +0000

    Create User Collection and create a post api for saving users information.

--- index.js
+++ index.js
@@ -22,15 +22,39 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     // Send a ping to confirm a successful connection
+
+    // create collection
+
+    const userCollection = client.db("Restaurant_DB").collection("users");
+    // create a post api for user collection
+
+    app.post("/users/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email };
+      const user = req.body;
+      // console.log(user);
+      const existUser = await userCollection.findOne(query);
+      if (existUser) {
+        // console.log("user exists before");
+        return res.send(existUser);
+      } else {
+        const result = await userCollection.insertOne({
+          user,
+        });
+        // console.log(result);
+        return res.send(result);
+      }
+    });
+
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
